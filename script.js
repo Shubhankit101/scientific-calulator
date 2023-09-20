d8e7b7d1b83bec54dc7a74f8ebb058f83f92a588
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-09-21 01:20:03 +0530

    theme change functionality

--- script.js
+++ script.js
@@ -2,11 +2,28 @@
 function clearScreen() {
     document.getElementById("result").value = "";
 }
-function display(value){
+
+function display(value) {
     document.getElementById("result").value += value;
 }
+
 function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
+    let p = document.getElementById("result").value;
+    let q = eval(p);
     document.getElementById("result").value = q;
 }
+const themeChangeEle = document.querySelector('.themeChange');
+
+//Theme change functionality
+themeChangeEle.addEventListener('click', function() {
+    let theme = document.querySelector('.themeChange').value;
+    if (theme === 'DarkMode') {
+        document.querySelector('.calculator').style.backgroundColor = '#212122';
+        document.querySelector('.themeChange').value = 'LightMode';
+        // #b0e9f5
+    } else {
+        document.querySelector('.calculator').style.backgroundColor = '#f8f6d5';
+        document.querySelector('.themeChange').value = 'DarkMode';
+    }
+
+})
